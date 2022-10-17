7c319bd7c58e0d9feb6f21448639a5bfe2ce99d5
Polina Pinchuk <polinapinchuck@gmail.com>
2022-10-17 22:03:03 +0200

    react-mesto-api-full

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const helmet = require('helmet');
 const router = require('./routes');
 const HandleErrors = require('./errors/HandleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { cors } = require('./middlewares/cors');
 
 const { PORT = 3000, LOCALHOST = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
@@ -24,39 +25,7 @@ app.use(requestLogger); // подключаем логгер запросов
 app.use(limiter);
 app.use(helmet());
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://polina.mesto.nomoredomains.icu',
-  'http://polina.mesto.nomoredomains.icu',
-  'localhost:3000',
-];
-
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-// eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  return next();
-});
+app.use(cors);
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,32 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://polina.mesto.nomoredomains.icu',
+  'http://polina.mesto.nomoredomains.icu',
+  'localhost:3000',
+];
+
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = {
+  cors,
+};
