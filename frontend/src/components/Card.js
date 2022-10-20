62ad6f543c7ba82c98d9aef76203e1c8a202d871
Polina Pinchuk <polinapinchuck@gmail.com>
2022-10-20 23:51:26 +0200

    react-mesto-api-full

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -13,6 +13,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   // const isLiked = props.card.likes.some(i => i._id === currentUser._id);
   const isLiked = card.likes.some((i) => i === currentUser._id);
+  console.log(isLiked)
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = (
     `element__button ${isLiked ? 'element__button_active' : ' '}`
